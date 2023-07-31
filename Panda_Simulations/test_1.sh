f7a77f3baa8fc214b3876e87203d639cb030fe70
jhoegerm <jhoegerm@vt.edu>
2023-07-31 11:31:21 -0400

    Reformat and Adding Panda Simulations

---
+++ Panda_Simulations/test_1.sh
@@ -0,0 +1,16 @@
+cd sim1
+python3 main_altTest.py
+python3 main_altTest2.py
+python3 main_altTest3.py
+
+cd ..
+cd sim2
+python3 mainAltTest.py
+python3 mainAltTest2.py
+python3 mainAltTest3.py
+
+cd ..
+cd sim3
+python3 mainAltTest.py
+python3 mainAltTest2.py
+python3 mainAltTest3.py
